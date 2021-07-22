2e13ae0dd99d28db27a7d3704a634c884b25319b
Nidhi Bhanushali <udemynb3study@gmail.com>
2021-07-22 18:28:21 +0530

    Errors resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(StartScreen());
+}
+
+class StartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Evento",
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(),
+      ),
+    );
+  }
 }
